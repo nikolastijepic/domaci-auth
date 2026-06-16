1ddde01954419b602f02f493e4fac12733c890e2
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-16 10:26:33 +0200

    Add layout & navigation

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/bootstrap-app.css', 'resources/js/bootstrap-app.js'])
+    <title>@yield('pageTitle')</title>
+</head>
+<body>
+@include('navigation')
+
+@yield('pageContent')
+</body>
+</html>
