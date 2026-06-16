9958d16ec81cb68aaa94db41be799255b3d73b06
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-16 08:35:07 +0200

    Setup Bootstrap with separate Vite assets

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+
+                'resources/css/bootstrap-app.css',
+                'resources/js/bootstrap-app.js',
+            ],
             refresh: true,
         }),
     ],
