e394e99758820937641e24f9569a7795158d6f5b
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-16 07:59:27 +0200

    Add Weather migration, controller, model & blade

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Weather;
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    //
+}
---
+++ app/Models/Weather.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Weather extends Model
+{
+    protected $table = 'weathers';
+    protected $fillable = [
+        'city',
+        'temperature',
+        ];
+}
