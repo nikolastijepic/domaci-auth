84418fbac108c5e320aa2aa7b59e081a2f8da7f4
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-17 14:22:13 +0200

    Add interactive user seeder

---
+++ database/seeders/InteractiveUserSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class InteractiveUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // NAME //
+        $name = $this->command->ask('Unesite ime');
+        if ($name === null || trim($name) === '') {
+            $this->command->error('Niste uneli ime!');
+            return;
+        }
+
+        // EMAIL //
+        $email = $this->command->ask('Unesite email');
+        if ($email === null || trim($email) === '') {
+            $this->command->error('Niste uneli email!');
+            return;
+        }
+        if (User::where('email', $email)->exists()) {
+            $this->command->error("Email $email vec postoji.");
+            return;
+        }
+
+        // PASSWORD //
+        $password = $this->command->ask('Unesite lozinka');
+        if ($password === null || trim($password) === '') {
+            $this->command->error('Niste uneli lozinku!');
+            return;
+        }
+
+        User::create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+        ]);
+
+        $this->command->info("Uspesno ste uneli podatke za $email.");
+    }
+}
