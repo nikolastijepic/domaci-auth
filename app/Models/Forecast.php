416881d0722699df2cfb8cc3d020df908baab4a1
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-18 15:18:08 +0200

    Add Forecast model and forecasts migration

---
+++ app/Models/Forecast.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Forecast extends Model
+{
+    protected $table = 'forecasts';
+
+    protected $fillable = [
+        'city_id',
+        'temperature',
+        'date',
+    ];
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+}
