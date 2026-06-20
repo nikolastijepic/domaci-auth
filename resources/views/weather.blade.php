1fc2e6b4392092cce3de638883278ad664582ecb
nikolastijepic <nikola.stijepic94@icloud.com>
2026-06-20 15:29:50 +0200

    feat: add links to city forecasts from weather page

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -10,20 +10,22 @@
     <div class="container mt-5">
         <div class="card shadow-sm">
             <div class="card-header">
-                <h4 class="mb-0">Weather Forecast</h4>
+                <h4 class="mb-0">Weather</h4>
             </div>
             <div class="card-body p-0">
                 <table class="table table-hover mb-0 align-middle">
                     <thead class="table-dark">
                     <tr>
-                        <th>City</th>
+                        <th>City (click for forecasts)</th>
                         <th>Temperature</th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach($weathers as $weather)
                         <tr>
-                            <td>{{ $weather->city->name }}</td>
+                            <td>
+                                <a href="{{ route('city.forecasts', $weather->city) }}">{{ $weather->city->name }} </a>
+                            </td>
                             <td>{{ $weather->temperature }} °C</td>
                         </tr>
                     @endforeach
